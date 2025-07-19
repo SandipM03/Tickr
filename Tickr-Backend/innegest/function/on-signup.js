4edd0a174680296693f590a5153f6a6476590ecf
SandipM03 <sandipmandal816708@gmail.com>
2025-07-19 20:08:24 +0530

    feat: implement user signup function with welcome email notification

--- Tickr-Backend/innegest/function/on-signup.js
+++ Tickr-Backend/innegest/function/on-signup.js
@@ -1,5 +1,6 @@
 import { innegest } from '../client.js';
 import { User } from '../../models/user.js'; // Assuming you have a User model defined
+import { sendMail } from '../../utils/mail.js';
 export const onUserSignup = innegest.createFunction({},
     {id:"on-user-signup", retries:2},
     {event: "app/signup"},
@@ -13,8 +14,18 @@ export const onUserSignup = innegest.createFunction({},
                }
                return userObject;
             });
+            await step.run("send-welcome-email", async () => {
+                const subject= `Welcome to Inngest`;
+                const message=`Hi
+                \n\n
+                Thanks for signing up. we're glad to have you onboard!`
+                await sendMail(user.email, subject, message);
+
+            })
+            return {success: true}
         }catch(error){
-            console.error("Error in onUserSignup:", error);
+            console.error("Error running step", error.message);
+            return {success: false}
         }
         
     }
