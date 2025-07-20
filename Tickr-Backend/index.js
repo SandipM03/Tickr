d0da670b37aa6fd6fde48a355ab8855995c8a06c
SandipM03 <sandipmandal816708@gmail.com>
2025-07-20 22:05:42 +0530

    feat: add ticket management routes and controller functions

--- Tickr-Backend/index.js
+++ Tickr-Backend/index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import userRoutes from './routes/user.js';
-
+import ticketRoutes from './routes/ticket.js';
+import {serve} from 'inngest/express';
+import {innegest} from './innegest/client.js';
+import {onUserSignup} from './innegest/function/on-signup.js';
+import {onTicketCreated} from './innegest/function/on-ticket.js';
 
 
 
@@ -10,15 +14,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth",userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use("/api/innegest",serve({
+    client: innegest,
+    functions: [onUserSignup, onTicketCreated],
+}))
+
 mongoose.connect(process.env.MONGO_URI)
  .then(()=>{
-
- 
     console.log("MongoDB connected ");
     app.listen(PORT,()=>console
     .log("server is  at http://localhost:3000"));
-    
-    
 })
  .catch((err)=>console.log("MongoDB  error:", err));
  
